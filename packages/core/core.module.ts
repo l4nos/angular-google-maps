8fa0e10faa7168d3b04e93e996d3709ffb975d89
Robert Lane <dev@carno.io>
2023-08-14 16:02:40 +0100

    added angular 16 support

--- packages/core/core.module.ts
+++ packages/core/core.module.ts
@@ -1,24 +1,24 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 
-import {AgmBicyclingLayer} from './directives/bicycling-layer';
-import {AgmCircle} from './directives/circle';
-import {AgmDataLayer} from './directives/data-layer';
-import {AgmFitBounds} from './directives/fit-bounds';
-import {AgmInfoWindow} from './directives/info-window';
-import {AgmKmlLayer} from './directives/kml-layer';
-import {AgmMap} from './directives/map';
-import {AgmMarker} from './directives/marker';
-import {AgmPolygon} from './directives/polygon';
-import {AgmPolyline} from './directives/polyline';
-import {AgmPolylineIcon} from './directives/polyline-icon';
-import {AgmPolylinePoint} from './directives/polyline-point';
-import {AgmRectangle} from './directives/rectangle';
-import {AgmTransitLayer} from './directives/transit-layer';
+import { AgmBicyclingLayer } from './directives/bicycling-layer';
+import { AgmCircle } from './directives/circle';
+import { AgmDataLayer } from './directives/data-layer';
+import { AgmFitBounds } from './directives/fit-bounds';
+import { AgmInfoWindow } from './directives/info-window';
+import { AgmKmlLayer } from './directives/kml-layer';
+import { AgmMap } from './directives/map';
+import { AgmMarker } from './directives/marker';
+import { AgmPolygon } from './directives/polygon';
+import { AgmPolyline } from './directives/polyline';
+import { AgmPolylineIcon } from './directives/polyline-icon';
+import { AgmPolylinePoint } from './directives/polyline-point';
+import { AgmRectangle } from './directives/rectangle';
+import { AgmTransitLayer } from './directives/transit-layer';
 
-import {LAZY_MAPS_API_CONFIG, LazyMapsAPILoader, LazyMapsAPILoaderConfigLiteral} from './services/maps-api-loader/lazy-maps-api-loader';
-import {MapsAPILoader} from './services/maps-api-loader/maps-api-loader';
+import { LAZY_MAPS_API_CONFIG, LazyMapsAPILoader, LazyMapsAPILoaderConfigLiteral } from './services/maps-api-loader/lazy-maps-api-loader';
+import { MapsAPILoader } from './services/maps-api-loader/maps-api-loader';
 
-import {BROWSER_GLOBALS_PROVIDERS} from './utils/browser-globals';
+import { BROWSER_GLOBALS_PROVIDERS } from './utils/browser-globals';
 
 /**
  * @internal
@@ -51,7 +51,7 @@ export class AgmCoreModule {
   /**
    * Please use this method when you register the module at the root level.
    */
-  static forRoot(lazyMapsAPILoaderConfig?: LazyMapsAPILoaderConfigLiteral): ModuleWithProviders {
+  static forRoot(lazyMapsAPILoaderConfig?: LazyMapsAPILoaderConfigLiteral): ModuleWithProviders<AgmCoreModule> {
     return {
       ngModule: AgmCoreModule,
       providers: [
